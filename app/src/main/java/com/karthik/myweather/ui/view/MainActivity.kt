909b16768b02ec5af3b081fcb8d8a61fb9eef15c
Karthik Gangineni <kgangineni@paypal.com>
2025-09-26 16:40:56 -0500

    release 3

--- app/src/main/java/com/karthik/myweather/ui/view/MainActivity.kt
+++ app/src/main/java/com/karthik/myweather/ui/view/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.main_activity.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    // Test Commit 3
+    // Test Commit 3, release 1.0.3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
